2afeaafe7856b166e1718b26fe773d30c48e89df
Davi Jesus <davijesusolive@gmail.com>
2026-08-04 09:21:42 -0300

    Add files via upload

---
+++ extriangulo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+	float base = 5, altura = 10, area;
+	
+	area = (base * altura) / 2;
+	
+	printf("A area do triangulo e: %.2f\n", area);
+	return 0;
+}
